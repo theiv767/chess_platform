dab98c882cad8d1e7a5c0143385b576cdb19b1f0
guimourf <gmourao071@gmail.com>
2023-06-12 23:36:43 -0300

    Login and Register Modal update

---
+++ server/routes/login.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+
+const username = document.getElementById(username)
--- view/scripts/modal.js
+++ view/scripts/modal.js
@@ -1,11 +1,38 @@
-function abrirModal(){
-    const modal = document.getElementById('janela-modal')
-    modal.classList.add('abrir')
+const modal = document.getElementById('janela-modal')
+const modalRegister = document.getElementById('janela-modal-registro')
+modal.classList.add('fechar')
+modalRegister.classList.add('fechar')
 
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'close-modal' || e.target.id == 'janela-modal'){
-            console.log("teste")
-            modal.classList.remove('abrir')
-        }
-    })
+function abrirModal(){
+    modal.classList.replace('fechar','abrir')
 }
+
+function logar() {
+    modal.classList.replace('abrir','fechar')
+    console.log('loguei')
+
+    //fazer os get e post
+}
+
+function registrar(){
+    modalRegister.classList.replace('abrir','fechar')
+    console.log('registrei')
+
+    //fazer os get e post
+}
+
+
+function registroModal(){
+    modal.classList.replace('abrir','fechar')
+    modalRegister.classList.replace('fechar','abrir')
+
+    console.log('fui pro registro')
+
+}
+function loginModal(){
+    modalRegister.classList.replace('abrir','fechar')
+    modal.classList.replace('fechar','abrir')
+
+    console.log('fui pro login')
+}
+
