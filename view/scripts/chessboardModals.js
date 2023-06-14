a6c68724ffb711cca93d01ae3fb3aa9d01abd2ac
guimourf <gmourao071@gmail.com>
2023-06-14 17:51:24 -0300

    Chess Timer and Modal done

--- view/scripts/chessboardModals.js
+++ view/scripts/chessboardModals.js
@@ -1,9 +1,20 @@
 const buscarModal = document.getElementById("buscar-partida")
 const buscandoModal = document.getElementById("buscando-partida")
+const modalResultado = document.getElementById("modal-partida")
+
+const blackpawn = document.getElementById("resultado-blacl")
+const whitepawn = document.getElementById("resultado-white")
+
 
 buscarModal.classList.add('abrir')
 buscandoModal.classList.add('fechar')
+modalResultado.classList.add('fechar')
 
+//quando for fazer a condição de abrir a modal coloca isso: modalResultado.classList.replace('fechar','abrir')
+function fecharResultado() {
+    modalResultado.classList.replace('abrir','fechar')
+
+}
 
 function buscar() {
     buscarModal.classList.replace('abrir','fechar')
