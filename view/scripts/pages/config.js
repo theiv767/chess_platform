c9cb24553b999abf1a147da25a5f13d6c98d8221
theiv <davib.g767@gmail.com>
2023-06-16 19:54:34 -0300

    feat img upload api

--- view/scripts/pages/config.js
+++ view/scripts/pages/config.js
@@ -1,5 +1,9 @@
 const pictureWrapper = document.querySelector("#pictureWrapper")
+const confirmBtn = document.querySelector("#confirmBtn")
+
+var formData = null;
 pictureWrapper.innerHTML = ''
+
 
 function updateImg() {
     if (pictureWrapper.innerHTML == '') {
@@ -27,8 +31,7 @@ function updateImg() {
                     const readerTarget = e.target;
                     const img = document.createElement("img");
                     img.src = readerTarget.result;
-                    console.log( img.src)
-                    
+
                     img.classList.add("pictureImg");
 
                     pictureImage.innerHTML = "";
@@ -36,19 +39,43 @@ function updateImg() {
                 });
 
                 reader.readAsDataURL(file);
+
+                formData = new FormData();
+                formData.append('name', "inputNome.value");
+                formData.append('file', file);
+
+
             } else {
                 pictureImage.innerHTML = pictureImageTxt;
             }
         });
+        console.log("input File: ")
+        console.log(inputFile.value)
+
 
     } else {
+        formData = null
         pictureWrapper.innerHTML = ''
     }
 
 }
 
+function confirm() {
+
+    //VERIFICANDO SE EXISTE ALGUM UPLOAD DE IMAGENS
+    if(formData != null){
+        axios.post('http://localhost:4000/pictures', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+    }else{
+        console.log("imagem não selecionada")
+    }
+}
+
+
+confirmBtn.onclick = confirm
 const alterImg = document.querySelector("#alterImg")
 alterImg.onclick = updateImg
-
-console.log(alterImg)
 
